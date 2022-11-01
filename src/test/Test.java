c33d46cb815d8768c3645452a8aef5088d2f898a
toyowatan <49113127+toyowatan@users.noreply.github.com>
2022-11-01 23:27:51 +0900

    Create Test.java

---
+++ src/test/Test.java
@@ -0,0 +1,34 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+public class Test {
+  public static void main(String[] args) {
+	  try {
+	    // データベースに接続
+	    Connection con = DriverManager.getConnection(
+	      "jdbc:mysql://192.168.100.4/test_database?useSSL=false",
+	      "root",
+	      "root"
+	    );
+	    // SQL文の実行
+	    String sql = "select * from test_table where id = '" + args[0] + "'";
+	    System.out.println(sql);
+	    PreparedStatement pstmt = con.prepareStatement(sql);
+	    ResultSet rs = pstmt.executeQuery();
+	    // 検索結果を表示
+	    while (rs.next()) {
+		      System.out.println("id:" +rs.getInt("id") + "/" + "name:" + rs.getString("name"));
+	    }
+	    // 後処理（リソースのクローズ）
+	    rs.close();
+	    pstmt.close();
+	    con.close();
+	  }
+	  catch ( Exception e ) {
+		  e.printStackTrace();
+	  }
+  }
+}
